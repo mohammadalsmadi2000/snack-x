84f9cec30a11a691f8b3a1f5cb0cf1624de7da8c
mohammadalsmadi2000 <mohammadalsmadi.fcbayern@gmail.com>
2023-06-21 18:51:13 +0000

    feat: 🚀 Initialize server

--- accounts/admin.py
+++ accounts/admin.py
@@ -9,6 +9,34 @@ class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ['email', 'username',]
+    list_display = ['email', 'username', 'first_name',]
+    fieldsets = UserAdmin.fieldsets + (('Contact Info', {'fields':('phone_number',)}),)
+
+    # fieldsets= (
+    #     ('Auth Info',{
+    #         'fields':('username','email','password'
+    #         )}
+    #     ),
+    #     ('personal Info',{
+    #         'fields':('first_name','last_name','phone_number'
+    #         )}
+    #     ),
+    #     ('Dates',{
+    #         'classes':('collapse',),
+    #         'fields':('date_joined','last_login'
+
+    #         )}
+    #     ),
+    #     ('permissions',{
+    #         'fields':(
+    #             'is_active',
+    #             'is_staff',
+    #             'is_superuser',
+    #             'user_permissions',
+    #             'groups')
+    #     }
+
+    #     )
+    # )
 
 admin.site.register(CustomUser, CustomUserAdmin)
--- django_project/settings.py
+++ django_project/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     # Local
     "accounts",
     "pages",
+    "snack",
 ]
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#middleware
